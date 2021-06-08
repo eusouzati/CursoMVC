849e0f8620a58f5c1022f0a39c01ad8293f02dc4
Eusouzati <silvatiago.0407@gmail.com>
2021-06-08 18:31:27 -0300

    CursoMVC

---
+++ CursoMVC/CursoMVC/Models/Produto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CursoMVC.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Descrição { get; set; }
+        public int Quantidade { get; set; }
+        public int CategoriaId { get; set; }
+        public Categoria Categoria { get; set; }
+    }
+
+}
---
+++ projetoASPNETMVC.md
@@ -0,0 +1,14 @@
+# Curso ASP.NET MVC :desktop_computer:
+
+#### Projeto desenvolvido no curso da Digital Innovation One.
+
+##### O que foi desenvolvido:
+
+- Criamos a primeira aplicação em ASP .NET MVC;
+
+  
+
+  
+
+
+
